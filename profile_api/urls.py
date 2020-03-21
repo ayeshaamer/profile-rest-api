b775fe19c36f5ce50a074ff9d23a04b82f39e6b2
Ayesha Amer Baig <ayeshaamer2016@gmail.com>
2020-03-21 23:40:38 +0500

    Added user profile feedAPI

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -7,10 +7,10 @@ from profile_api import views
 router = DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet, base_name="hello-viewset")
 router.register('profile',views.UserProfileViewSet)
+router.register('feed',views.UserProfileFeedViewSet)
 
 urlpatterns = [
 path('hello-view/', views.HelloApiView.as_view(),name="HelloView"),
 path('login/', views.UserLoginApiView.as_view()),
 path('', include(router.urls)),
-
 ]
