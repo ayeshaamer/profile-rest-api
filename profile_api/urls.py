5902a2d5a571bb9ba090e9bcc99be99973269849
Ayesha Amer Baig <ayeshaamer2016@gmail.com>
2020-03-21 22:14:32 +0500

    Added profiles viewset

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -6,8 +6,10 @@ from profile_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet, base_name="hello-viewset")
+router.register('profile',views.UserProfileViewSet)
 
 urlpatterns = [
 path('hello-view/', views.HelloApiView.as_view(),name="HelloView"),
-path('', include(router.urls))
+path('', include(router.urls)),
+
 ]
